581b709dc8230bb24d855c0bac6e96b249f9a242
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-27 06:40:14 -0500

    Probando prototipos

--- 1-organizing-JS-code/objetcs-and-constructors.js
+++ 1-organizing-JS-code/objetcs-and-constructors.js
@@ -14,3 +14,22 @@ const book2 = new Book("My pobre angelito", "no se", 34, false);
 console.log(book1.info() + "\n" + book2.info());
 
 console.log(Object.getPrototypeOf(book1) === Book.prototype);
+
+let Hamster = {
+  estomago: [],
+  comer: function(comida) {
+    this.estomago = [comida];
+  }
+};
+
+let speedy = {
+  __proto__: Hamster
+};
+
+let lazy = {
+  __proto__: Hamster
+};
+
+speedy.comer("manzana");
+console.log(speedy.estomago);
+console.log(lazy.estomago);
