690ed02bb3fe64e02ff134b7e953203e242ff404
JhonatanDczel <jariasq@unsa.edu.pe>
2024-02-10 23:08:28 -0500

    Sube cambios olvidados de webpack

--- 1-organizing-JS-code/webpack-practice-course/src/index.js
+++ 1-organizing-JS-code/webpack-practice-course/src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import mysql from 'mysql';
 
 function component() {
   const element = document.createElement('div');
--- 1-organizing-JS-code/webpack-practice-course/src/print.js
+++ 1-organizing-JS-code/webpack-practice-course/src/print.js
@@ -1,3 +1,5 @@
 export default function printMe() {
   console.log('I get called from print.js!');
+  console.log("i am grood");
+  alert("holaaa");
 }
