1a45879ebb4f176b1ef4861b260096776389409f
JhonatanDczel <jariasq@unsa.edu.pe>
2024-02-10 23:18:13 -0500

    Ejemplo de codigo formateado con Linter

--- 2-JavaScript-in-the-real-world/linting/index.js
+++ 2-JavaScript-in-the-real-world/linting/index.js
@@ -1,12 +1,10 @@
 // Este es un ejemplo de un codigo escrito y formateado con linter
 
-if(true)
-
-
-{
-  console.log("Hola mundo");
+let condition = true
+if (condition) {
+  console.log('Hola mundo')
 }
-let x = 1;
-let y = 2;
-let z = x + y;
-console.log(z);
+let x = 1
+let y = 2
+let z = x + y
+console.log(z)
